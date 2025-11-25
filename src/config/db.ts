9848e4a2bcac3f7db313d87327ff83a7ec4add51
Solbucreate <mortensolbu@gmail.com>
2025-11-25 11:39:25 +0100

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,31 +1,14 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD as string,
+  process.env.DB_NAME || "",
+  process.env.DB_USER || "",
+  process.env.DB_PASS || "",
   {
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 5432,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-    define: {
-      freezeTableName: true,
-    },
-    logging: console.log, // hjelp med debugging
+    dialect: "mysql",
+    logging: false,
   }
 );
 
-export const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("✅ Connected to Render PostgreSQL!");
-  } catch (error) {
-    console.error("❌ DB connection error:", error);
-  }
-};
+export default sequelize;
