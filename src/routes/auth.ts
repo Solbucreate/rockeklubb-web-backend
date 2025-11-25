c905f993c310a2e25e0f756235883b141fdbc98f
Solbucreate <mortensolbu@gmail.com>
2025-11-25 11:20:49 +0100

    Delete src directory

--- src/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-import { Application } from "express";
-import * as authcontroller from "../controllers/authcontroller";
-import { authMiddleware } from "../middleware/authMiddleware";
-
-export default function authRoutes(app: Application) {
-  app.post("/api/auth/login", authcontroller.login);
-  app.get("/api/auth/profile", authMiddleware, authcontroller.getProfile);
-}
--- src/routes/orders.ts
+++
@@ -1,7 +0,0 @@
-import { Application } from "express";
-import * as orderscontroller from "../controllers/orderscontroller";
-
-export default function ordersRoutes(app: Application) {
-  app.get("/api/orders/:id", orderscontroller.getOrderById);
-  app.post("/api/orders", orderscontroller.createOrder);
-}
