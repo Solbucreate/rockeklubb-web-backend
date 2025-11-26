d99bfce3ec1996b3b39f9bd08eec5ff3f3d29822
Morten Solbu <solbucreate@gmail.com>
2025-11-26 12:03:12 +0100

    Switch to bcryptjs for Render compatibility

--- src/controllers/authcontroller.ts
+++ src/controllers/authcontroller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import User from "../models/user";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const login = async (req: Request, res: Response) => {
